0f279e7acd381daedec01425951c5acd876e7420
William Selbie <selbiew@oregonstate.edu>
2017-11-16 15:30:40 -0800

    Added memory storage for MPU6050

--- WiFi-Motes/AP_LOOM_Ishield_OSCtransmit/AP_LOOM_Ishield_OSCtransmit.ino
+++ WiFi-Motes/AP_LOOM_Ishield_OSCtransmit/AP_LOOM_Ishield_OSCtransmit.ino
@@ -53,16 +53,16 @@ to
 #define MEM_EEPROM 1
 
 #ifndef MEM_TYPE
-#define MEM_TYPE MEM_FLASH
+  #define MEM_TYPE MEM_FLASH
 #endif  
 
 #if MEM_TYPE == MEM_FLASH
-#include <FlashStorage.h>
+  #include <FlashStorage.h>
 #endif 
 
 // Define your sensor type here by commenting and un-commenting
 #define is_analog 2      // also define number of channels
-//#define is_i2c 0x68      // also define i2c address of device
+#define is_i2c 0x68      // also define i2c address of device
 #define transmit_butt 10        // using on-board button, specify attached pin, transmitting 
 #define VBATPIN A7       // Pin to check for battery voltage
 //#define CLIENT_REQUESTS_DATA 1 // Set to 1 if you only send data when requested by client, else, send data at sample/sleep rate
@@ -88,6 +88,8 @@ to
 #endif
 
 //ADD sparkfun library for is_sleep_interrupt
+
+//Structure to store device configuration information
 struct config_t {
   byte checksum;               //value is changed when flash memory is written to.
   IPAddress ip;                //Device's IP Address
@@ -101,14 +103,12 @@ struct config_t {
   int   ax_offset, ay_offset, az_offset, gx_offset, gy_offset, gz_offset; //mpu6050 config
 #endif
 };
-
 struct config_t configuration;
+const byte memValidationValue = 99; // Value to test to see if memory has been written before
 
 #if MEM_TYPE == MEM_FLASH
-FlashStorage(flash_config,config_t);    //Setup the flash storage for the structure
+  FlashStorage(flash_config,config_t);    //Setup the flash storage for the structure
 #endif
-
-const byte flashValidationValue = 99; // Value to test to see if flashMem has been written before
 
 int led =  LED_BUILTIN;
 volatile bool ledState = LOW;
@@ -143,68 +143,68 @@ WiFiServer server(80);
 #endif
 
 #ifdef is_i2c
-// Include libraries for serial and i2c devices
+  // Include libraries for serial and i2c devices
   //#include "S_message.h"
   #include "I2Cdev.h"
   #include "Wire.h"
   #include "MPU6050_6Axis_MotionApps20.h"
   MPU6050 mpu; // create instance of MPU6050 called mpu
   MPU6050 accelgyro; // another called accelgyro
-// Uncoment one or more of these to determine which readings and format to send
+  // Uncoment one or more of these to determine which readings and format to send
   #define OUTPUT_READABLE_YAWPITCHROLL
   //#define OUTPUT_BINARY_YAWPITCHROLL
   //#define OUTPUT_READABLE_REALACCEL
   //#define OUTPUT_READABLE_WORLDACCEL  // this is pretty cool
   //#define OUTPUT_BINARY_ACCELGYRO
-#define INTERRUPT_PIN 11  // use pin 2 on Arduino Uno, pin 3 on Adafruit ProTrinket!
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-// Place to save accel gyro mpu6050 readings
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-
-
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-    mpuInterrupt = true;
-}
-
-// MPU calibration vars:
-int mean_ax,mean_ay,mean_az,mean_gx,mean_gy,mean_gz,state=0;
-//int ax_offset,ay_offset,az_offset,gx_offset,gy_offset,gz_offset;
-//Change this 3 variables if you want to fine tune the skecth to your needs.
-int buffersize=1000;     //Amount of readings used to average, make it higher to get more precision but sketch will be slower  (default:1000)
-int acel_deadzone=8;     //Acelerometer error allowed, make it lower to get more precision, but sketch may not converge  (default:8)
-int giro_deadzone=1;     //Giro error allowed, make it lower to get more precision, but sketch may not converge  (default:1)
-
-// ================================================================
-// ===       Struct for saving MPU params       ===
-// ================================================================
-// see http://playground.arduino.cc/Code/EEPROMWriteAnything
-/*struct config_t
-{
-    int ax_offset,ay_offset,az_offset,gx_offset,gy_offset,gz_offset;
-    byte written;
-} configuration;*/
+  #define INTERRUPT_PIN 11  // use pin 2 on Arduino Uno, pin 3 on Adafruit ProTrinket!
+  // MPU control/status vars
+  bool dmpReady = false;  // set true if DMP init was successful
+  uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+  uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+  uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+  uint16_t fifoCount;     // count of all bytes currently in FIFO
+  uint8_t fifoBuffer[64]; // FIFO storage buffer
+  
+  // orientation/motion vars
+  Quaternion q;           // [w, x, y, z]         quaternion container
+  VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+  VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+  VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+  VectorFloat gravity;    // [x, y, z]            gravity vector
+  float euler[3];         // [psi, theta, phi]    Euler angle container
+  float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+  
+  // Place to save accel gyro mpu6050 readings
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+  
+  
+  // ================================================================
+  // ===               INTERRUPT DETECTION ROUTINE                ===
+  // ================================================================
+  
+  volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+  void dmpDataReady() {
+      mpuInterrupt = true;
+  }
+  
+  // MPU calibration vars:
+  int mean_ax,mean_ay,mean_az,mean_gx,mean_gy,mean_gz,state=0;
+  //int ax_offset,ay_offset,az_offset,gx_offset,gy_offset,gz_offset;
+  //Change this 3 variables if you want to fine tune the skecth to your needs.
+  int buffersize=1000;     //Amount of readings used to average, make it higher to get more precision but sketch will be slower  (default:1000)
+  int acel_deadzone=8;     //Acelerometer error allowed, make it lower to get more precision, but sketch may not converge  (default:8)
+  int giro_deadzone=1;     //Giro error allowed, make it lower to get more precision, but sketch may not converge  (default:1)
+  
+  // ================================================================
+  // ===       Struct for saving MPU params       ===
+  // ================================================================
+  // see http://playground.arduino.cc/Code/EEPROMWriteAnything
+  /*struct config_t
+  {
+      int ax_offset,ay_offset,az_offset,gx_offset,gy_offset,gz_offset;
+      byte written;
+  } configuration;*/
 #endif
 // ** End if i2c check
 
@@ -215,65 +215,11 @@ void setup() {
   while(!Serial) { }		 //Ensure Serial is ready to go before anything happens.
 #endif
   pinMode(led, OUTPUT);      // set the LED pin mode
-
-#if MEM_TYPE == MEM_FLASH
-  #if DEBUG == 1
-    Serial.println("Reading from flash.");
-  #endif
-  configuration = flash_config.read();                    // read from flash memory
-  #if DEBUG == 1
-    Serial.print("Checksum: ");
-    Serial.println(configuration.checksum);
-  #endif
-  if (configuration.checksum != flashValidationValue){    //
-		configuration.ssid = "wifi101-network";              // created AP name
-        configuration.pass = "1234567890";                // AP password (needed only for WEP, must be exactly 10 or 26 characters in length)
-        configuration.keyIndex = 0;                       // your network key Index number (needed only for WEP)
-        configuration.ip_broadcast = "192.168.1.255";     // IP to Broadcast data 
-        configuration.localPort = 9436;                   // local port to listen on
-        #ifdef is_i2c
-        calMPU6050();                                     // auto-calibrate the MPU6050; if this succeeds, it also writes configuration.checksum=flashValidationValue
-        #else
-        configuration.checksum = flashValidationValue;    // configuration has been written successfully, so we write the checksum
-        #endif
-  #if DEBUG == 1
-        Serial.println("Writing to flash for the first time.");
-  #endif
-        flash_config.write(configuration);                //don't uncomment this line until we're pretty confident that this behaves how we want; flash memory has limited writes and we don't want to waste it on unnecessary tests
-  }
-#endif
-#if DEBUG == 1          //If the read from memory is successful.
-  else {
-	Serial.print("SSID: ");
-	Serial.println(configuration.ssid);
-	Serial.print("Pass: ");
-	Serial.println(configuration.pass);
-	Serial.print("Key Index: ");
-	Serial.println(configuration.keyIndex);
-	Serial.print("IP to Broadcast to: ");
-	Serial.println(configuration.ip_broadcast);
-	Serial.print("Local Port: ");
-	Serial.println(configuration.localPort);
-#ifdef is_i2c
-	Serial.print("AX Offset: ");
-	Serial.println(configuration.ax_offset);
-	Serial.print("AY Offset: ");
-	Serial.println(configuration.ay_offset);
-	Serial.print("AZ Offset: ");
-	Serial.println(configuration.az_offset);
-	Serial.print("GX Offset: ");
-	Serial.println(configuration.gx_offset);
-	Serial.print("GY Offset: ");
-	Serial.println(configuration.gy_offset);
-	Serial.print("GZ Offset: ");
-	Serial.println(configuration.gz_offset);
-#endif
-  }
-#endif
   
 #ifdef transmit_butt
   pinMode(transmit_butt, INPUT_PULLUP);      // set the transmit_butt pin mode to input
 #endif
+
 #ifdef is_i2c
   // join I2C bus (I2Cdev library doesn't do this automatically)
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -283,9 +229,9 @@ void setup() {
         Fastwire::setup(400, true);
     #endif
     pinMode(INTERRUPT_PIN, INPUT);
-#if DEBUG == 1
-  Serial.println("Using MPU6050 i2c");
-#endif
+  #if DEBUG == 1
+    Serial.println("Using MPU6050 i2c");
+  #endif
 // *** Init MPU 6050 and serial stuff
   accelgyro.initialize();
   mpu.initialize();
@@ -298,15 +244,15 @@ void setup() {
 // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
         // turn on the DMP, now that it's ready
-#if DEBUG == 1
+    #if DEBUG == 1
         Serial.println(F("Enabling DMP..."));
-#endif
+    #endif
         mpu.setDMPEnabled(true);
 
         // Uncomment following 2 lines if using enable Arduino Uno, MO, or Trinket interrupt detection
-#if DEBUG == 1
-		Serial.println(F("Enabling MPU interrupt detection (Arduino external interrupt 0)..."));
-#endif
+    #if DEBUG == 1
+  		Serial.println(F("Enabling MPU interrupt detection (Arduino external interrupt 0)..."));
+    #endif
         attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
 
         // Uncomment following 2 lines if using Adafruit Feather 32u4
@@ -319,9 +265,9 @@ void setup() {
         mpuIntStatus = mpu.getIntStatus();
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
-#if DEBUG == 1
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-#endif
+        #if DEBUG == 1
+            Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        #endif
         dmpReady = true;
 
         // get expected DMP packet size for later comparison
@@ -331,15 +277,73 @@ void setup() {
         // 1 = initial memory load failed
         // 2 = DMP configuration updates failed
         // (if it's going to break, usually the code will be 1)
-#if DEBUG == 1
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
-#endif
+        #if DEBUG == 1
+            Serial.print(F("DMP Initialization failed (code "));
+            Serial.print(devStatus);
+            Serial.println(F(")"));
+        #endif
     }
-  // Auto Calibrate MPU6050 on startup. Change this to save calibration values in flash and only trigger function when received calibrate request from client
-// ** end serial stuff
- #endif 
+  // Auto Calibrate MPU6050 on startup. Change this to save calibration values in flash and only trigger function when received calibrate request from client  
+  //calMPU6050();
+  // ** end serial stuff
+#endif 
+
+
+#if MEM_TYPE == MEM_FLASH
+  #if DEBUG == 1
+    Serial.println("Reading from flash.");
+  #endif
+  configuration = flash_config.read();                    // read from flash memory
+  #if DEBUG == 1
+    Serial.print("Checksum: ");
+    Serial.println(configuration.checksum);
+  #endif
+  if (configuration.checksum != memValidationValue){    //The memory has not been written to before. 
+    configuration.ssid = "wifi101-network";              // created AP name
+    configuration.pass = "1234567890";                // AP password (needed only for WEP, must be exactly 10 or 26 characters in length)
+    configuration.keyIndex = 0;                       // your network key Index number (needed only for WEP)
+    configuration.ip_broadcast = "192.168.1.255";     // IP to Broadcast data 
+    configuration.localPort = 9436;                   // local port to listen on
+    #ifdef is_i2c
+      calMPU6050();
+    #else
+      configuration.checksum = memValidationValue;      // configuration has been written successfully, so we write the checksum
+    #endif
+  #if DEBUG == 1
+        Serial.println("Writing to flash for the first time.");
+  #endif
+        flash_config.write(configuration);                //don't uncomment this line until we're pretty confident that this behaves how we want; flash memory has limited writes and we don't want to waste it on unnecessary tests
+  }
+#endif
+#if DEBUG == 1          //If the read from memory is successful.
+  else {
+    Serial.print("SSID: ");
+    Serial.println(configuration.ssid);
+    Serial.print("Pass: ");
+    Serial.println(configuration.pass);
+    Serial.print("Key Index: ");
+    Serial.println(configuration.keyIndex);
+    Serial.print("IP to Broadcast to: ");
+    Serial.println(configuration.ip_broadcast);
+    Serial.print("Local Port: ");
+    Serial.println(configuration.localPort);
+    #ifdef is_i2c
+      Serial.print("ax_offset: ");
+      Serial.println(configuration.ax_offset);
+      Serial.print("ay_offset: ");
+      Serial.println(configuration.ay_offset);
+      Serial.print("az_offset: ");
+      Serial.println(configuration.az_offset);
+      Serial.print("gx_offset: ");
+      Serial.println(configuration.gx_offset);
+      Serial.print("gy_offset: ");
+      Serial.println(configuration.gy_offset);
+      Serial.print("gz_offset: ");
+      Serial.println(configuration.gz_offset);
+    #endif
+  }
+#endif
+ 
 //----------------------------------------------------
 // -----              WiFi AP Setup              -----
 //----------------------------------------------------
@@ -373,9 +377,9 @@ void setup() {
   // Create open network. Change this line if you want to create an WEP network:
   status = WiFi.beginAP(configuration.ssid);
   if (status != WL_AP_LISTENING) {
-#if DEBUG == 1
-    Serial.println("Creating access point failed");
-#endif
+    #if DEBUG == 1
+      Serial.println("Creating access point failed");
+    #endif
     // don't continue
     while (true);
   }
@@ -388,8 +392,7 @@ void setup() {
 #if DEBUG == 1
   // you're connected now, so print out the status
   printWiFiStatus();
-
-  Serial.println("\nStarting UDP connection over server...");
+  Serial.println("Starting UDP connection over server...");
 #endif
   // if you get a connection, report back via serial:
   Udp.begin(configuration.localPort);
