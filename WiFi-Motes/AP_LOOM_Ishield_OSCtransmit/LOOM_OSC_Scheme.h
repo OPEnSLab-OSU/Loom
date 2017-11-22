7fae9369768aeef73a589c36cccafc7a933f79a0
William Selbie <selbiew@oregonstate.edu>
2017-11-22 10:11:10 -0800

    Is_i2c enabled by default. Added LOOM_OSC_Scheme.h
    is_i2c is no longer commented out.
    LOOM_OSC_Scheme.h is included to make use of preprocessor string concatenation
    and IDString definition.
    Network SSID defaults to the IDString, defined in LOOM_OSC_Scheme.h

---
+++ WiFi-Motes/AP_LOOM_Ishield_OSCtransmit/LOOM_OSC_Scheme.h
@@ -0,0 +1,4 @@
+#define STR_(x) #x
+#define STR(x) STR_(x) //to concatenate a predefined number to a string literal, use STR(x)
+
+#define IDString FAMILY DEVICE STR(INSTANCE_NUM) // C interprets subsequent string literals as concatenation: "/Loom" "/Ishield" "0" becomes "/Loom/Ishield0"
