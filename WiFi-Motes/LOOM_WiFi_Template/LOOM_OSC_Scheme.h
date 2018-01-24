2f38c37ac16172cfbcaa7cfeb090b922a5f9e27b
swopet <swopet@oregonstate.edu>
2018-01-24 14:06:42 -0800

    remove old scheme header

--- WiFi-Motes/LOOM_WiFi_Template/LOOM_OSC_Scheme.h
+++
@@ -1,5 +0,0 @@
-#define STR_(x) #x
-#define STR(x) STR_(x) //to concatenate a predefined number to a string literal, use STR(x)
-
-#define IDString FAMILY DEVICE STR(INSTANCE_NUM) // C interprets subsequent string literals as concatenation: "/Loom" "/Ishield" "0" becomes "/Loom/Ishield0"
-//more detail to come here
