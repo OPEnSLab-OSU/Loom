25311291549cb72b2ee2ed91d70d70656f4a1efd
William Selbie <selbiew@oregonstate.edu>
2018-01-30 16:36:33 -0800

    Removed mpu6050 code from decagon sketch

--- WiFi-Motes/AP_decagon_OSCtransmit/AP_decagon_OSCtransmit.ino
+++ WiFi-Motes/AP_decagon_OSCtransmit/AP_decagon_OSCtransmit.ino
@@ -38,26 +38,18 @@ to
 #include <WiFi101.h>
 #include <WiFiUdp.h>
 #include <OSCBundle.h>
-
+#include "LOOM_OSC_Scheme.h"
 
 // Define your sensor type here by commenting and un-commenting
-//#define is_analog 2      // also define number of channels
-//#define is_i2c 0x68      // also define i2c address of device
 #define transmit_butt 10        // using on-board button, specify attached pin, transmitting 
 #define VBATPIN A7       // Pin to check for battery voltage
-//#define CLIENT_REQUESTS_DATA 1 // Set to 1 if you only send data when requested by client, else, send data at sample/sleep rate
 
 #define TRANSMISSION_PERIOD 6000     //The number of milliseconds between when the decagon finishes transmitting
-                            // and starts the reading again   
-                                              
-#define STR_HELPER(x) #x
-#define STR(x) STR_HELPER(x) //to concatenate a predefined number to a string literal, use STR(x)
-
+                                     // and starts the reading again   
 #define FAMILY "/LOOM"
 #define DEVICE "/Decagon"
 #define INSTANCE_NUM 0  // Unique instance number for this device, useful when using more than one of the same device type in same space
 
-#define IDString FAMILY DEVICE STR(INSTANCE_NUM) // C interprets subsequent string literals as concatenation: "/Loom" "/Ishield" "0" becomes "/Loom/Ishield0"
 // Set Sleep Mode Use one or the other or neither of the following 2 lines
 #define is_sleep_period 50  // Uncomment to use SleepyDog to transmit at intervals up to 16s and sleep in between
 //#define is_sleep_interrupt 11  // Uncoment to use Low-Power library to sit in idle sleep until woken by pin interrupt, parameter is pin to interrupt
@@ -89,81 +81,10 @@ WiFiUDP Udp;
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
 
-// OSC Address Builder String:
-//const String IDstring = "/LOOM/Ishield" + INSTANCE_NUM; // example "/LOOM/Ishield0"
-//uint8_t addrStringSize = sizeof(IDstring);
-char  IDbuffer[] = "/LOOM/Ishield0";
-
 #ifdef is_analog
   #define num_measurements 4 // must be 1, 2, 4, or 8)! number of analog measurements to sample and average per channel
   int16_t a0, a1, a2, a3, a4, a5; // Memory to store analog sensor values
 #endif
-
-#ifdef is_i2c
-// Include libraries for serial and i2c devices
-  //#include "S_message.h"
-  #include "I2Cdev.h"
-  #include "Wire.h"
-  #include "MPU6050_6Axis_MotionApps20.h"
-  MPU6050 mpu; // create instance of MPU6050 called mpu
-  MPU6050 accelgyro; // another called accelgyro
-// Uncoment one or more of these to determine which readings and format to send
-  #define OUTPUT_READABLE_YAWPITCHROLL
-  //#define OUTPUT_BINARY_YAWPITCHROLL
-  //#define OUTPUT_READABLE_REALACCEL
-  //#define OUTPUT_READABLE_WORLDACCEL  // this is pretty cool
-  //#define OUTPUT_BINARY_ACCELGYRO
-#define INTERRUPT_PIN 11  // use pin 2 on Arduino Uno, pin 3 on Adafruit ProTrinket!
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-// Place to save accel gyro mpu6050 readings
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-
-
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-    mpuInterrupt = true;
-}
-
-// MPU calibration vars:
-int mean_ax,mean_ay,mean_az,mean_gx,mean_gy,mean_gz,state=0;
-//int ax_offset,ay_offset,az_offset,gx_offset,gy_offset,gz_offset;
-//Change this 3 variables if you want to fine tune the skecth to your needs.
-int buffersize=1000;     //Amount of readings used to average, make it higher to get more precision but sketch will be slower  (default:1000)
-int acel_deadzone=8;     //Acelerometer error allowed, make it lower to get more precision, but sketch may not converge  (default:8)
-int giro_deadzone=1;     //Giro error allowed, make it lower to get more precision, but sketch may not converge  (default:1)
-
-// ================================================================
-// ===       Struct for saving MPU params       ===
-// ================================================================
-// see http://playground.arduino.cc/Code/EEPROMWriteAnything
-struct config_t
-{
-    int ax_offset,ay_offset,az_offset,gx_offset,gy_offset,gz_offset;
-    byte written;
-} configuration;
-#endif
-// ** End if i2c check
 
 // ================================================================
 // ===         Decagon Inclusions               ===
@@ -192,62 +113,7 @@ void setup() {
 #ifdef transmit_butt
   pinMode(transmit_butt, INPUT_PULLUP);      // set the transmit_butt pin mode to input
 #endif
-#ifdef is_i2c
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-        Fastwire::setup(400, true);
-    #endif
-    pinMode(INTERRUPT_PIN, INPUT);
-  Serial.println("Using MPU6050 i2c");
-// *** Init MPU 6050 and serial stuff
-  accelgyro.initialize();
-  mpu.initialize();
-  devStatus = mpu.dmpInitialize();
-  // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
-// make sure it worked (returns 0 if so)
-    if (devStatus == 0) {
-        // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
 
-        // Uncomment following 2 lines if using enable Arduino Uno, MO, or Trinket interrupt detection
-        Serial.println(F("Enabling MPU interrupt detection (Arduino external interrupt 0)..."));
-        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-
-        // Uncomment following 2 lines if using Adafruit Feather 32u4
-        // enable interrupt for PCINT7...
-        //Serial.println(F("Enabling MPU interrupt detection PCINT 7 (pin 11)"));
-        //pciSetup(INTERRUPT_PIN);
-        
-        mpuIntStatus = mpu.getIntStatus();
-
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
-
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
-    }
-  // Auto Calibrate MPU6050 on startup. Change this to save calibration values in flash and only trigger function when received calibrate request from client
-  calMPU6050();
-// ** end serial stuff
-
-#endif 
 //----------------------------------------------------
 // -----              WiFi AP Setup              -----
 //----------------------------------------------------
@@ -325,26 +191,6 @@ void loop() {
     }
   }
 
-#if (CLIENT_REQUESTS_DATA)
-  // if there's data available, read a packet
-  int packetSize = Udp.parsePacket();
-  if (packetSize)
-  {
-    Serial.print("Received packet of size ");
-    Serial.println(packetSize);
-    Serial.print("From ");
-    IPAddress remoteIp = Udp.remoteIP();
-    Serial.print(remoteIp);
-    Serial.print(", port ");
-    Serial.println(Udp.remotePort());
-
-    // read the packet into packetBufffer
-    int len = Udp.read(packetBuffer, 255);
-    if (len > 0) packetBuffer[len] = 0;
-    Serial.println("Contents:");
-    Serial.println(packetBuffer);
-#endif
-
 // measure battery voltage
   vbat = analogRead(VBATPIN);
   vbat *= 2;    // we divided by 2, so multiply back
@@ -369,14 +215,6 @@ void loop() {
     udp_decagon();
 
     delay((uint32_t)TRANSMISSION_PERIOD);
-#endif
-    
-#ifdef is_analog
-    measure_analog();
-#endif
-
-#if (CLIENT_REQUESTS_DATA)
-  } // end if(packetsize) check (true if client sends request for data)
 #endif
 
 #ifdef is_sleep_period
