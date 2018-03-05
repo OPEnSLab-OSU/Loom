ad1b36c05116c581f90d45211266a221ea596342
William Selbie <selbiew@oregonstate.edu>
2018-03-05 15:08:19 -0800

    Bundle sending with spreadsheet update

---
+++ LoRa/LoRa_OSCReceiveHub/test_pb.py
@@ -0,0 +1,26 @@
+# importing the requests library
+from collections import OrderedDict
+import requests
+ 
+# api-endpoint
+devid_v = "v25CCAAB0F709665"
+id_tag_v = "3"
+timestamp_v = "2018"
+temp_c_v = "4C"
+humid_v = "33"
+load_cell_v = "1000"
+ir_light_v = "2000"
+full_light_v = "3000"
+bat_volt_v = "4.2"
+
+payload = (('devid', devid_v), ('IDtag', id_tag_v), ('TimeStamp', timestamp_v),
+           ('TempC', temp_c_v), ('Humid', humid_v), ('LoadCell', load_cell_v),
+           ('IRLight', ir_light_v), ('FullLight', full_light_v), ('BatVolt', bat_volt_v))
+
+URL = "http://api.pushingbox.com/pushingbox?" 
+ 
+# sending get request and saving the response as response object
+r = requests.get(url = URL, params = payload)
+print r.url
+
+
