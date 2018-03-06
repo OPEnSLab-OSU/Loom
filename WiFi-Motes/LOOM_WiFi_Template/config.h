d9e626cfe6e425e0078a2438350e5144d695a8f2
Luke Goertzen <goertzel@oregonstate.edu>
2018-03-06 15:37:11 -0800

    Latest version

--- WiFi-Motes/LOOM_WiFi_Template/config.h
+++ WiFi-Motes/LOOM_WiFi_Template/config.h
@@ -1,5 +1,5 @@
 #define FAMILY "LOOM"    // Should always be "LOOM", you can change this if you are setting up your own network
-#define DEVICE "RelayShield"  // The device name, i.e. "RelayShield" or "IShield"
+#define DEVICE "Ishield"  // The device name, should begin with a slash followed by an unbroken string with no more slashes i.e. "RelayShield" or "IShield"
 
 
 
@@ -14,22 +14,32 @@
 #define RECEIVE_OSC       // Comment this out to turn off receiving of OSC messages
 
 //#define is_servo
-#define is_relay
-//#define is_analog 2
-//#define is_i2c 0x86
+
+
+#define A0            14 
+#define A1            15 
+#define A2            16 
+
+#define is_analog 2
+#define is_i2c 0x86
 #ifdef is_i2c
   #define is_mpu6050
   #define is_neopixel
 #endif
-#ifndef is_relay
-//#define transmit_butt 10        // using on-board button, specify attached pin, transmitting 
+
+#ifdef is_neopixel
+  #define Neo0 0
+  #define Neo1 0
+  #define Neo2 1
 #endif
+
+#define transmit_butt 10        // using on-board button, specify attached pin, transmitting 
 #define VBATPIN A7       // Pin to check for battery voltage
 
 #define is_sleep_period 50 // Uncomment to use SleepyDog to transmit at intervals up to 16s and sleep in between. Change the value according to the length of your desired transmission interval
 //#define is_sleep_interrupt 11 // Uncomment to use Low-Power library to sit in idle sleep until woken by pin interrupt, parameter is pin to interrupt
 
 #define INIT_PORT 9436
-#define INIT_INST 0
-#define AP_NAME "featherM0"
+#define INIT_INST 7
+#define AP_NAME "featherM0-7"
 
