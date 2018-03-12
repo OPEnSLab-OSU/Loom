1fed6b4e9443c939212fa791e6bfa5e569507e7c
William Selbie <selbiew@oregonstate.edu>
2018-03-11 17:27:16 -0700

    Added dynamic key/value sending

--- LoRa/LoRa_OSCReceiveHub/test_pb.py
+++ LoRa/LoRa_OSCReceiveHub/test_pb.py
@@ -13,21 +13,18 @@ ir_light_v = "2000"
 full_light_v = "3000"
 bat_volt_v = "4.2"
 
-keyList = ['devid', 'IDtag', 'TimeStamp', 'TempC', 'Humid', 'loadcell', 'irlight', 'fullight',
+keyList = ['IDtag', 'TimeStamp', 'TempC', 'Humid', 'loadcell', 'irlight', 'fullight',
            'batvolt']
-valList = [devid_v, id_tag_v, timestamp_v, temp_c_v, humid_v, load_cell_v, ir_light_v,
+valList = [id_tag_v, timestamp_v, temp_c_v, humid_v, load_cell_v, ir_light_v,
            full_light_v, bat_volt_v]
 
 param_dict = OrderedDict()
+param_dict['devid'] = devid_v
 for i in range(len(keyList)):
     param_dict['key' + str(i)] = keyList[i]
     param_dict['val' + str(i)] = valList[i]
 
-payload = (('devid', devid_v), ('IDtag', id_tag_v), ('TimeStamp', timestamp_v),
-           ('TempC', temp_c_v), ('Humid', humid_v), ('LoadCell', load_cell_v),
-           ('IRLight', ir_light_v), ('FullLight', full_light_v), ('BatVolt', bat_volt_v))
-
-URL = "http://api.pushingbox.com/pushingbox?" 
+URL = "http://api.pushingbox.com/pushingbox?"
  
 # sending get request and saving the response as response object
 r = requests.get(url = URL, params = param_dict)
