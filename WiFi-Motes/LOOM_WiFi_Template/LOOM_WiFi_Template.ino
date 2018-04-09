6f2b53452f4b1c8049c347d8de0a0adb8763e49d
swopet <swopet@oregonstate.edu>
2018-04-09 13:43:33 -0700

    add preliminary preamble file

--- WiFi-Motes/LOOM_WiFi_Template/LOOM_WiFi_Template.ino
+++ WiFi-Motes/LOOM_WiFi_Template/LOOM_WiFi_Template.ino
@@ -29,28 +29,7 @@ enum WiFiMode{
 
 // Config has to be first
 #include "config.h"
-#include "declarations_and_structures.h"
-
-// Using Config defines, check which headers to include
-#ifdef is_analog
-  #include "analogIn_subroutines.h"
-#endif
-#ifdef is_i2c
-  #include "mpu6050_subroutines.h"
-#endif
-#ifdef is_neopixel
-  #include "neopixel_subroutines.h"
-#endif
-#ifdef is_servo  
-  #include "servo_subroutines.h"
-#endif
-#ifdef is_relay
-  #include "relay_subroutines.h"
-#endif
-
-// These may refer to functions in above headers
-#include "common_subroutines.h"
-#include "wifi_subroutines.h"
+#include "preamble.h"
 
 
 
