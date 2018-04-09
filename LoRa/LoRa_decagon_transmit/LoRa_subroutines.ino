93f1800e3cddc2237c2d475a81ee1964c9139ad5
noblek <noblek@oregonstate.edu>
2018-04-09 15:28:12 -0700

    Added battery voltage and time stamp readings

--- LoRa/LoRa_decagon_transmit/LoRa_subroutines.ino
+++ LoRa/LoRa_decagon_transmit/LoRa_subroutines.ino
@@ -20,6 +20,8 @@ void package_data(OSCBundle *bndl, struct SensorList data, int sensor_number) {
   if (data.type[sensor_number] == D_GS3) {
     msg->add("IDtag").add("Decagon" STR(INSTANCE_NUM));
     msg->add("Instance").add((int32_t)sensor_number);
+    msg->add("Timestamp").add(TimeStamp);
+    msg->add("BatVolt").add((float)measuredvbat);
     msg->add("VWC").add((float)data.readings[sensor_number][0]);
     msg->add("Temp").add((float)data.readings[sensor_number][1]);
     msg->add("ElecCond").add((float)data.readings[sensor_number][2]);
