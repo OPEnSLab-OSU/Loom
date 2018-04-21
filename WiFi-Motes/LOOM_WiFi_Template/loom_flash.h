d2b68930870fb002ae476268b89732a8f597649e
swopet <swopet@oregonstate.edu>
2018-04-21 13:14:33 -0700

    move init_config() to loom_flash.h

--- WiFi-Motes/LOOM_WiFi_Template/loom_flash.h
+++ WiFi-Motes/LOOM_WiFi_Template/loom_flash.h
@@ -1,3 +1,4 @@
+#define memValidationValue 99
 // Define struct to hold DEVICE CONFIG SETTINGS
 // The structure that gets stored in non-volatile memory to keep settings
 struct config_flash_t {
@@ -40,6 +41,64 @@ void read_non_volatile();
     EEPROM_writeAnything(0,configuration);
   }
 #endif
+
+// --- INITIAL CONFIG ---
+// Handles configuration of reading device config from flash (if applicable)
+//   else uses config.h specific settings, and writes these to flash
+// Arguments: none
+// Return:    none
+void init_config()
+{
+  #if is_wifi == 1
+    packet_header_string = configuration.packet_header_string;
+    link_config_wifi(&configuration.config_wifi);
+  #endif
+  #if is_mpu6050 == 1
+    link_config_mpu6050(&configuration.config_mpu6050);
+  #endif
+  #if MEM_TYPE == MEM_FLASH || MEM_TYPE == MEM_EEPROM
+    read_non_volatile(); //reads configuration from non_volatile memory
+    
+    #if DEBUG == 1
+      Serial.println("Reading from non-volatile memory...");
+      Serial.print("Checksum: ");
+      Serial.println(configuration.checksum);
+    #endif
+    
+    if (configuration.checksum != memValidationValue) {     // Write default values to flash
+          configuration.instance_number = INIT_INST;
+          sprintf(configuration.packet_header_string,"%s%d\0",PacketHeaderString,configuration.instance_number);
+          #if DEBUG == 1
+            Serial.print("expecting OSC header ");
+            Serial.println(configuration.packet_header_string);
+          #endif
+          
+          #if is_wifi == 1
+            configuration.config_wifi.my_ssid = AP_NAME;                  // Default AP name
+            strcpy(configuration.config_wifi.ssid,DEFAULT_NETWORK);       // Default network name
+            strcpy(configuration.config_wifi.pass,DEFAULT_PASSWORD);      // AP password (needed only for WEP, must be exactly 10 or 26 characters in length)
+            configuration.config_wifi.keyIndex = 0;                       // Your network key Index number (needed only for WEP)
+            configuration.config_wifi.ip_broadcast = "255.255.255.255";     // IP to Broadcast data 
+            configuration.config_wifi.localPort = INIT_PORT;              // Local port to listen on
+            configuration.config_wifi.wifi_mode = DEFAULT_MODE;           // WiFi mode to start in (AP_MODE, WPA_CLIENT_MODE, WEP_CLIENT_MODE)
+          #endif
+          // Add any other behavior/calibration wrapped in an #ifdef is_something preprocessor directive HERE
+          
+          #if is_mpu6050 == 1
+            calMPU6050();                                     // Calibration writes memValidationValue for us
+          #endif
+          
+          configuration.checksum = memValidationValue;      // Configuration has been written successfully, so we write the checksum
+          
+
+          #if DEBUG == 1
+            Serial.println("Writing to flash for the first time.");
+          #endif
+          write_non_volatile();
+                                                              // Flash memory has limited writes and we don't want to waste it on unnecessary tests
+    } // of if (configuration.checksum != memValidationValue)
+    #endif //of MEM_TYPE
+}
 
 
 
