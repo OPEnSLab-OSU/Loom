5cbe02977eca3dd95a0434bd89dfce975d758a5c
goertzel <goertzel@oregonstate.edu>
2018-04-21 16:06:07 -0700

    Updated Wifi file format

--- WiFi-Motes/LOOM_WiFi_Template/config.h
+++ WiFi-Motes/LOOM_WiFi_Template/config.h
@@ -6,12 +6,12 @@
 //--OPTIONS--//
 #define DEBUG 1   // Set to 1 if you want Serial statements from various functions to print
 
-#define is_ishield 0
+#define is_ishield 1
 #define num_servos 0
 #define is_relay 0
-#define is_wifi 0
-#define is_lora 1
-#define is_analog 0
+#define is_wifi 1
+#define is_lora 0
+#define is_analog 2
 
 //Multiplexer
 #define is_tca9548a 1
