8066ad527fca75a0f3bcdbeff8e628629276b6ff
goertzel <goertzel@oregonstate.edu>
2018-06-06 17:30:44 -0700

    Removing obsolete WiFi-Motes folder

--- WiFi-Motes/AP_LOOM_Ishield_OSCtransmit_GyroToPixel/LOOM_OSC_Scheme.h
+++
@@ -1,7 +0,0 @@
-#define STR_(x) #x
-#define STR(x) STR_(x) //to concatenate a predefined number to a string literal, use STR(x)
-
-#define IDString "feather"
-
-//Delimits the FAMILY, DEVICE, and INSTANCE_NUM with '/' for use in OSC Bundles
-#define PacketHeaderString STR(/) FAMILY STR(/) DEVICE STR(INSTANCE_NUM)
