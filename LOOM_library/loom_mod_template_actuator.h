8ebee0fcb411caf071c318003dd3166d0b293a8d
goertzel <goertzel@oregonstate.edu>
2018-08-15 09:24:50 -0700

    Added separate actuator template

--- LOOM_library/loom_mod_template_actuator.h
+++ LOOM_library/loom_mod_template_actuator.h
@@ -34,8 +34,7 @@ struct state_<module>_t  state_<module>;
 // ===                   FUNCTION PROTOTYPES                    === 
 // ================================================================
 bool setup_<module>();
-void package_<module>(OSCBundle *, char[]);
-void measure_<module>();
+void handle_<module>(OSCMessage &msg);
 
 
 // ================================================================ 
@@ -48,27 +47,7 @@ void measure_<module>();
 //
 bool setup_<module>() 
 {
-	//Setup Here
 
-
-	// Example of what setup might look like:
-
-	// bool is_setup;
-	// state_<module>.float = 1.2345;
-	
-	// if(<some setup condition>) {
-	// 	is_setup = true;
-	// 	#if LOOM_DEBUG == 1
-	// 		Serial.println("Setup <module> successful.");
-	// 	#endif
-	// }
-	// else {
-	// 	is_setup = false;
-	// 	#if LOOM_DEBUG == 1
-	// 		Serial.println("Failed to setup <module>.");
-	// 	#endif
-	// }
-	// return is_setup;
 }
 
 
@@ -77,59 +56,36 @@ bool setup_<module>()
 // ================================================================
 
 
-// --- PACKAGE <MODULE> --- (Multiplexer Version)
+// --- HANDLE <MODULE> --- (Multiplexer Version)
 //
-// Adds OSC Message of most recent sensor readings to a provided OSC bundle
+// Takes OSC Message forwarded by the message router from another
+// device or the Max interfaces
 //
-// @param bndl                  The OSC bundle to be added to
-// @param packet_header_string  The device-identifying string to prepend to OSC messages
-//   if I2C multiplexer sensor, then also
-// [@param port                  Which port of the multiplexer the device is plugged into]
-//
-void package_<module>(OSCBundle *bndl, char packet_header_string[], int port) 
+// @param msg  The message containing an actuator command
+void handle_<module>(OSCMessage &msg) 
 {
-	// Create a message and fill it here, then add it to the bndl
-
 	// Example of what measure might look like:
 
-	// char address_string[255];
-	// sprintf(address_string, "%s%s", packet_header_string, "/<module>");
-	// OSCMessage msg = OSCMessage(address_string);
-	// msg.add("ex1").add(state_<module>.array_example[0]);
-	// msg.add("ex2").add(state_<module>.array_example[1]);
-	// msg.add("ex3").add(state_<module>.array_example[2]);
-	// bndl->add(msg);
+	int module_num  = msg.getInt(0);
+	int amount = msg.getInt(1);
+	
+	LOOM_DEBUG_Print("received message for <module ");
+	LOOM_DEBUG_Print(module_num);
+	LOOM_DEBUG_Print(" with degree ");
+	LOOM_DEBUG_Println(amount);
+	
+
+	// Use the received value to set / control the actuator 
+	//  in some way
+	// This can be done here or, preferred, done via an auxiliary
+	//  function that take individual value rather than an 
+	//  OSC Message
+
+	set_module(module_num, amount);
+
 }
 
-void package_<module>(OSCBundle *bndl, char packet_header_string[])
-{
-	char address_string[255];
 
-	sprintf(address_string, "%s%s", packet_header_string, "/module_example0");
-	bndl->add(address_string).add(state_<module>.array_example[0]);
-	sprintf(address_string, "%s%s", packet_header_string, "/module_example1");
-	bndl->add(address_string ).add(state_<module>.array_example[1]);
-}
-
-
-
-// --- MEASURE <MODULE> ---
-//
-// Gets the current sensor readings of the <Module and stores into variables
-// If debug prints are enabled, can also print any relevant information 
-//
-void measure_<module>() 
-{
-	// Measure data and change the state here. 
-	// Potentially uses the config data
-
-	// Example of what measure might look like:
-
-	// state_<module>.some_value = ReadSensorValue
-	// for (int i = 0; i < 3) {
-	// 		state_<module>.array_example[i] = ReadOtherValue
-	// }
-}
 
 
 
