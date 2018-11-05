9bc6d457630fd3dc856f6d4b3f7cfc303591abb8
goertzel <goertzel@oregonstate.edu>
2018-11-05 14:49:58 -0800

    Updated .ino example

--- LOOM_library/LOOM_library.ino
+++ LOOM_library/LOOM_library.ino
@@ -31,28 +31,24 @@ void setup()
 void loop() 
 {
 
+
 	// --- Common Example ---
 	OSCBundle bndl, send_bndl;  		// Declare bundles to hold incoming and outgoing data
 
-	// receive_bundle(&bndl, WIFI);		// Receive messages
-	// if (bndl.size()) {
-	// 	print_bundle(&bndl);			// Print bundle if LOOM_DEBUG enabled
-	// }
-	// process_bundle(&bndl);				// Dispatch message to correct handling functions
+	receive_bundle(&bndl, WIFI);		// Receive messages
+	if (bndl.size()) {
+		print_bundle(&bndl);			// Print bundle if LOOM_DEBUG enabled
+	}
+	process_bundle(&bndl);				// Dispatch message to correct handling functions
 
 	measure_sensors();					// Read sensors, store data in sensor state struct
 	package_data(&send_bndl);			// Copy sensor data from state to provided bundle
 	
-	print_bundle(&send_bndl);
-
-	// send_bundle(&send_bndl, WIFI);		// Send bundle of packaged data
-	// log_bundle(&send_bndl, PUSHINGBOX);
-	log_bundle(&send_bndl, OLED);
-
+	send_bundle(&send_bndl, WIFI);		// Send bundle of packaged data
+	log_bundle(&send_bndl, PUSHINGBOX);	// Send bundle to Google Sheet
 
 	additional_loop_checks();			// Miscellaneous checks
 	// --- End Example ---
-
 }
 
 
@@ -60,17 +56,16 @@ void loop()
 // ===                 High-Level API Functions                 === 
 // ================================================================ 
 
-//
 // void receive_bundle(OSCBundle *bndl, CommPlatform platform);
 // void process_bundle(OSCBundle *bndl);
 // void measure_sensors();
 // void package_data(OSCBundle *send_bndl);
 // void send_bundle(OSCBundle *send_bndl, CommPlatform platform, int port);
 // void send_bundle(OSCBundle *send_bndl, CommPlatform platform);
-// void log_bundle(OSCBundle *send_bndl, LogPlatform platform, char* file); //filename for SD files
+// void log_bundle(OSCBundle *send_bndl, LogPlatform platform, char* file); // filename required for SD files
 // void log_bundle(OSCBundle *send_bndl, LogPlatform platform);
 // bool bundle_empty(OSCBundle *bndl);
 // void additional_loop_checks();
 
 // CommPlatforms: WIFI, LORA, NRF
-// LogPlatforms:  PUSHINGBOX, SDCARD
+// LogPlatforms:  PUSHINGBOX, SDCARD, OLED
