3b739f759990959632679cf32267c6fe6ee64dc9
goertzel <goertzel@oregonstate.edu>
2018-11-19 16:20:35 -0800

    Added evaporometer example

--- Instances_of_Loom/Ghana_Hackathon/evap_LOOM_library.ino
+++ Instances_of_Loom/Ghana_Hackathon/evap_LOOM_library.ino
@@ -33,35 +33,6 @@ void setup()
 void loop() 
 {
 
-	// // --- LoRa Hub Example ---
-	// OSCBundle bndl;  		// Declare bundles to hold incoming and outgoing data
-
-	// receive_bundle(&bndl, LORA);		// Read sensors, store data in sensor state struct
-	// print_bundle(&bndl);
-	// log_bundle(&bndl, PUSHINGBOX);
-
-	// additional_loop_checks();			// Miscellaneous checks
-	// // --- End Example ---
-
-
-
-	// // --- LoRa Node Example ---
-	// OSCBundle bndl, send_bndl;  		// Declare bundles to hold incoming and outgoing data
-
-	// measure_sensors();					// Read sensors, store data in sensor state struct
-	// package_data(&send_bndl);			// Copy sensor data from state to provided bundle
-	
-	// print_bundle(&send_bndl);
-
-	// log_bundle(&send_bndl, SDCARD, "savefile.csv");
-	// send_bundle(&send_bndl, LORA);
-
-	// delay(1000);
-
-	// additional_loop_checks();			// Miscellaneous checks
-	// // --- End Example ---
-
-
 	// --- LoRa Evaporometer Example ---
 	OSCBundle bndl, send_bndl;  		// Declare bundles to hold incoming and outgoing data
 
@@ -79,26 +50,6 @@ void loop()
 	// --- End Example ---
 
 
-
-
-
-	// // --- Common Example ---
-	// OSCBundle bndl, send_bndl;  		// Declare bundles to hold incoming and outgoing data
-
-	// receive_bundle(&bndl, WIFI);		// Receive messages
-	// if (bndl.size()) {
-	// 	print_bundle(&bndl);			// Print bundle if LOOM_DEBUG enabled
-	// }
-	// process_bundle(&bndl);				// Dispatch message to correct handling functions
-
-	// measure_sensors();					// Read sensors, store data in sensor state struct
-	// package_data(&send_bndl);			// Copy sensor data from state to provided bundle
-	
-	// send_bundle(&send_bndl, WIFI);		// Send bundle of packaged data
-	// log_bundle(&send_bndl, PUSHINGBOX);	// Send bundle to Google Sheet
-
-	// additional_loop_checks();			// Miscellaneous checks
-	// // --- End Example ---
 }
 
 
