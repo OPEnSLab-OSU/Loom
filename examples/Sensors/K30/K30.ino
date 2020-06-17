f226972c629adb0885455d8bc6e5ee08108a4650
MrKangs <gykang00@gmail.com>
2020-06-16 21:03:36 -0700

    modify the K30 Example

--- examples/Sensors/K30/K30.ino
+++ examples/Sensors/K30/K30.ino
@@ -1,11 +1,12 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // This is basic example to use the K30 sensor with Loom.
-// The K30 sensor will measure the CO2 from the range 0 to 10000.
+// The K30 sensor will measure the CO2 from the range 0 to 10000 ppm and log to SD.
 
 // In this example of program, it will measure the CO2 level and display on the Serial Monitor.
 // Note that the K30 sensor needs at least 6 minutes of warm up to get accurate measurements.
-// Therefore, make sure let it on for 6 minutes to warm up the K30 sensor before record data values. 
+// Therefore, you will get data values after 6 minutes have passed.
+// Inside this program, there is a timer that will run for 6 minutes before it starts measuring values. 
 
 // As you might see, there are some syntax that came from outside of the Loom language, 
 // so take a look the link and the code carefully.  
@@ -36,6 +37,7 @@ LoomManager Loom{ &ModuleFactory };
 // Create Serial SERCOM for K30 Sensor: RX pin 12, TX pin 11
 Uart Serial2 = Uart(&sercom1, 12, 11, SERCOM_RX_PAD_3, UART_TX_PAD_0);
 
+int timer = 0; // Setting up the timer
 
 void setup() {
   
@@ -52,17 +54,37 @@ void setup() {
   Loom.K30().set_serial(&Serial2);
 
   LPrintln("\n ** Setup Complete ** ");
+
 }
 
 void loop() {  
+
+  if (timer == 0){ // This will only pass once at the beginning to start the timer for warm up
+    warmUpTimer();
+    timer = 1; // Once the timer is done, it will never do the time again
+  }
   
   Loom.measure(); // Sample attached sensors
   Loom.package(); // Format data for display and SD
   Loom.display_data(); // display printed JSON formatted data on serial monitor
+  Loom.SDCARD().log(); // Loggin K30 Data value into SDCard
   Loom.pause();
 }
 
-void SERCOM1_Handler()
-{
+void SERCOM1_Handler(){ // This function is require for the K30 Serial Sensor because of UART Type
   Serial2.IrqHandler();
+}
+
+void warmUpTimer(){ // This function is a timer to warm up the K30 sensor to get accurate measurements
+  
+  LPrintln("\n ** Set up 6 minutes Warm Up time to get accurate measurements ** ");
+
+  for(int timePassed = 1; timePassed < 7; timePassed++){ // By pausing Loom, it will not measure CO2 value for 6 minutes
+    Loom.pause(60000); // The max is only 1 min for pause, we loop it for 6 times to make it 6 minutes
+    LPrint(timePassed); // Knowing the User that how many minutes have been passed
+    LPrint(" minute(s) passed!");
+    LPrint("\n");
+  }
+  
+  LPrintln("\n ** Ready to Measure ** ");
 }
--- examples/Sensors/K30/config.h
+++ examples/Sensors/K30/config.h
@@ -1,7 +1,8 @@
 "{\
-  'general':{'name':'Device', 'instance':1,'interval':5000},\
+  'general':{'name':'K30', 'instance':1,'interval':5000},\
   'components':[\
-    {'name':'K30','params':'default'}\
+    {'name':'K30','params':'default'},\
+    {'name':'SD','params':[true,1000,10,'K30_',true]}\
   ]\
 }"
     
