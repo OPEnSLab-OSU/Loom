0104f7da50ec1a3a8ce552038017e7e83523e1c8
akerr501 <akerr501@gmail.com>
2020-07-01 11:16:38 -0700

    Reverting setup.h

--- setup/setup.sh
+++ setup/setup.sh
@@ -18,12 +18,36 @@ arduino_setup()
 
 	# Install Adafruit SAMD board profiles
 	arduino-cli core install adafruit:samd --additional-urls https://adafruit.github.io/arduino-board-index/package_adafruit_index.json
-	arduino-cli core install adafruit:samd --additional-urls https://github.com/OPEnSLab-OSU/Loom_Auxiliary/raw/RefactorV3/package_loom_index.json
 
 	# Create Arduino folder
 	mkdir -p ~/Documents/Arduino/libraries
 }
 
+# Download regular Loom dependencies
+download_loom_dependencies()
+{
+	# Install Loom dependencies (from library manager
+	curl -fsSL https://raw.githubusercontent.com/OPEnSLab-OSU/Loom/master/setup/library_list.txt | xargs -n1 arduino-cli lib install
+}
+
+# Download Loom dependencies not in library manager
+download_extra_loom_dependencies()
+{
+	# Install Loom dependencies (not in library manager)
+	curl -fsSL https://raw.githubusercontent.com/OPEnSLab-OSU/Loom/master/setup/library_list2.txt | xargs -n1 -I % sh -c '{ curl -sL % | tar xC ~/Documents/Arduino/libraries/; sleep 1; }'
+}
+
+# Install Loom and its dependencies
+install_loom()
+{
+	# Get Loom and install Loom
+	# curl -sL https://github.com/OPEnSLab-OSU/Loom/archive/1.0.0.zip | tar xC ~/Documents/Arduino/libraries/
+	curl -sL https://github.com/OPEnSLab-OSU/Loom/archive/master.zip | tar xC ~/Documents/Arduino/libraries/
+
+	# Install dependencies
+	download_loom_dependencies
+	download_extra_loom_dependencies
+}
 
 # Open link to download IDE
 open_ide_link()
@@ -35,12 +59,21 @@ open_ide_link()
 	echo "If it did not, open it here: https://www.arduino.cc/en/main/software"
 }
 
+# Run any post install cleanup
+cleanup()
+{
+	# Remove possible __MACOSX folder
+	rm -R ~/Documents/Arduino/libraries/__MACOSX
+}
 
 # Pre-exit behavior if signal caught
 terminate()
 {
-	echo "Caught Signal ... quitting"
+	echo "Caught Signal ... cleaning up."
 
+	cleanup
+
+	echo "Done cleanup ... quitting."
 	exit 1
 }
 
@@ -51,4 +84,6 @@ terminate()
 trap terminate 1 2 3 6
 download_arduino_cli
 arduino_setup
+install_loom
 open_ide_link
+cleanup
