97153fe49af3489bf5d83c1639fc0b9c717b7a12
Kenneth Kang <kangken@oregonstate.edu>
2020-07-31 13:58:59 -0700

    Update config.h
    Better format

--- examples/Logging Platforms/BatchSD/config.h
+++ examples/Logging Platforms/BatchSD/config.h
@@ -14,40 +14,32 @@
 			'name':'SD',\
 			'params':[true,1000,10,'data',true]\
 		},\
-    {\
-      'name':'BatchSD',\
-      'params': [true, 1000, 10]\
-    },\
+    		{\
+      			'name':'BatchSD',\
+      			'params': [true, 1000, 10]\
+    		},\
 		{\
 			'name':'LTE',\
 			'params':['<apn>','<username>','<password>', '<analog_pin>']\
-			},\
-	    {\
-	      'name':'GoogleSheets',\
-	      'params':[\
-	        'Goog',\
-	        7003,\
-	        '/macros/s/<your-script-id>/exec',\
+		},\
+	    	{\
+	      		'name':'GoogleSheets',\
+	      		'params':['Goog', 7003,'/macros/s/<your-script-id>/exec',\
 	        '<your-sheet-id>',\
 	/*true to autoname tab*/				true,\
 	/*not used if previous param is true*/	'<your-tab-name>'\
-	      ]\
-	    }\
-/*    {\
-      'name':'nRF',\
-      'params':[120,01,1,0,3,200,1]\
-    },\
-    {\
+	      			]\
+	    	}\
+		
+/*    		{\
+      			'name':'nRF',\
+      			'params':[120,01,1,0,3,200,1]\
+    		},\
+    		{\
 			'name':'LoRa',\
-			'params':[\
-				255,\
-				0,\
-				23,\
-				3,\
-				200\
-			]\
+			'params':[255, 0, 23, 3, 200]\
 		},\
-    {\
+    		{\
 			'name':'Bluetooth',\
 			'params':'default'\
 		}\
