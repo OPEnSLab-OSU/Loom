e464e96bd954dc0d27e26813a0230e45e4f6f11f
Bryson Goto <55998145+BGoto808@users.noreply.github.com>
2020-08-17 11:13:37 -1000

    Fixed comments / added power rail global variables

--- examples/Lab Examples/Hypnos_SD/Hypnos_SD.ino
+++ examples/Lab Examples/Hypnos_SD/Hypnos_SD.ino
@@ -19,28 +19,29 @@ const char* json_config =
 #include "config.h"
 ;
 
+#define HYPNOS3 5
+#define HYPNOS5 6
+
 // Set enabled modules
 LoomFactory<
 	Enable::Internet::Disabled,
 	Enable::Sensors::Enabled,
 	Enable::Radios::Enabled,
 	Enable::Actuators::Enabled,
-	Enable::Max::Enabled
+	Enable::Max::Disabled
 > ModuleFactory{};
 
 LoomManager Loom{ &ModuleFactory };
 
-
-
 void setup() 
 {
 	// Needs to be done for Hypno Board
-	pinMode(5, OUTPUT);		// Enable control of 3.3V rail 
-	pinMode(6, OUTPUT);		// Enable control of 5V rail 
+	pinMode(HYPNOS3, OUTPUT);		// Enable control of 3.3V rail 
+	pinMode(HYPNOS5, OUTPUT);		// Enable control of 5V rail 
 
 	//See Above
-	digitalWrite(5, LOW);	// Enable 3.3V rail
-	digitalWrite(6, HIGH);	// Enable 5V rail
+	digitalWrite(HYPNOS3, LOW);	// Enable 3.3V rail
+	digitalWrite(HYPNOS5, HIGH);	// Enable 5V rail
 
 	Loom.begin_serial(true);
 	Loom.parse_config(json_config);
