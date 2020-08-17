548e4aec5a396d3bd265899281490e34c5e6a715
Bryson Goto <55998145+BGoto808@users.noreply.github.com>
2020-08-17 11:14:50 -1000

    Fixed comments / added power rail global variables

--- examples/Lab Examples/Hypnos_Sheets_LTE_Sleep/Hypnos_Sheets_LTE_Sleep.ino
+++ examples/Lab Examples/Hypnos_Sheets_LTE_Sleep/Hypnos_Sheets_LTE_Sleep.ino
@@ -5,6 +5,10 @@ const char* json_config =
 #include "config.h"
 ;
 
+#define RTC_INT_PIN 12
+#define HYPNOS3 5
+#define HYPNOS5 6
+
 // Set enabled modules
 LoomFactory<
 	Enable::Internet::LTE,
@@ -20,21 +24,21 @@ volatile bool rtc_flag = false;
 
 void wakeISR_RTC() {
 	// disable the interrupt
-	detachInterrupt(12);
+	detachInterrupt(RTC_INT_PIN);
 	rtc_flag = true;
 }
 
 void setup()
 {
 	// Needs to be done for Hypno Board
-	pinMode(5, OUTPUT);		// Enable control of 3.3V rail
-	pinMode(6, OUTPUT);		// Enable control of 5V rail
-	pinMode(12, INPUT_PULLUP);		// Enable waiting for RTC interrupt, MUST use a pullup since signal is active low
+	pinMode(HYPNOS3, OUTPUT);		// Enable control of 3.3V rail
+	pinMode(HYPNOS5, OUTPUT);		// Enable control of 5V rail
+	pinMode(RTC_INT_PIN, INPUT_PULLUP);		// Enable waiting for RTC interrupt, MUST use a pullup since signal is active low
   pinMode(13, OUTPUT);
 
 	//See Above
-	digitalWrite(5, LOW);	// Enable 3.3V rail
-	digitalWrite(6, HIGH);	// Enable 5V rail
+	digitalWrite(HYPNOS3, LOW);	// Enable 3.3V rail
+	digitalWrite(HYPNOS5, HIGH);	// Enable 5V rail
   digitalWrite(13, LOW);
 
 	Loom.begin_serial(true);
@@ -43,7 +47,7 @@ void setup()
 
   delay(5000);
 	// Register an interrupt on the RTC alarm pin
-	Loom.InterruptManager().register_ISR(12, wakeISR_RTC, LOW, ISR_Type::IMMEDIATE);
+	Loom.InterruptManager().register_ISR(RTC_INT_PIN, wakeISR_RTC, LOW, ISR_Type::IMMEDIATE);
   delay(5000);
 
 	LPrintln("\n ** Setup Complete ** ");
@@ -53,8 +57,8 @@ void setup()
 
 void loop(){
   digitalWrite(13, HIGH);
-  digitalWrite(5, LOW); // Enable 3.3V rail
-  digitalWrite(6, HIGH);  // Enable 5V rail
+  digitalWrite(HYPNOS3, LOW); // Enable 3.3V rail
+  digitalWrite(HYPNOS5, HIGH);  // Enable 5V rail
 
 // As it turns out, if the SD card is initialized and you change
 // the states of the pins to ANY VALUE, the SD card will fail to
@@ -78,12 +82,12 @@ Loom.GoogleSheets().publish();
 
 // set the RTC alarm to a duration of five minutes with TimeSpan
 Loom.InterruptManager().RTC_alarm_duration(TimeSpan(0,0,5,0));
-Loom.InterruptManager().reconnect_interrupt(12);
+Loom.InterruptManager().reconnect_interrupt(RTC_INT_PIN);
 
 
 digitalWrite(13, LOW);
-digitalWrite(5, HIGH); // Disable 3.3V rail
-digitalWrite(6, LOW);  // Disable 5V rail
+digitalWrite(HYPNOS3, HIGH); // Disable 3.3V rail
+digitalWrite(HYPNOS5, LOW);  // Disable 5V rail
 
 pinMode(23, INPUT);
 pinMode(24, INPUT);
