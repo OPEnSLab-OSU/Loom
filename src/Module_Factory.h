c6916c44676c01f2383ed710c670cc99d8097766
Luke Goertzen <goertzel@oregonstate.edu>
2020-09-15 08:27:48 -0700

    Add print_factory() to registry, clean up file

--- src/Module_Factory.h
+++ src/Module_Factory.h
@@ -20,133 +20,156 @@
 #include <vector>
 
 
-///Forward declare any classes that will be served to created objects
+///Forward declare any classes that will be served the created objects
 // class Scope;
 class LoomManager;
 
-/// Creates a new T, returned as S*. S must be base of T
-/// @return The created T
-template <class S, class T>
-S *ConstructDefault(LoomManager *manager) { return new T(manager); }
+///////////////////////////////////////////////////////////////////////////////
 
 /// Creates a new T, returned as S*. S must be base of T
 /// @return The created T
 template <class S, class T>
-S *ConstructDefaultJson(LoomManager* manager, JsonArrayConst p) { return new T(manager, p); }
+S* ConstructDefault(LoomManager *manager) { return new T(manager); }
 
+/// Creates a new T, returned as S*. S must be base of T
+/// @return The created T
+template <class S, class T>
+S* ConstructDefaultJson(LoomManager* manager, JsonArrayConst p) { return new T(manager, p); }
+
+///////////////////////////////////////////////////////////////////////////////
 /// A generic registry/factory that collects constructors of a give base type and allows for any number
 /// of base classes as static singleton templates will only create new instances of a new type is introduced
 template <typename T>
 class Registry
 {
+
 public:
 	using FactoryFunction = T *(*)(LoomManager*); ///< Pointer to function of type //* void T::funct (void) *//
 	using FactoryFunctionJson = LoomModule *(*)(LoomManager*, JsonArrayConst);
 	using FactoryPair = struct
 	{										///<  *Needed as an alternative to std::map
-		const char *name;					///< Name of module that will be used to make a new copy
+		const char* name;					///< Name of module that will be used to make a new copy
 		const FactoryFunction ctor;			///< Pointer to the Creation function which will be used to CTOR
 		const FactoryFunctionJson ctorJson; ///< Pointer to the CreationJSON function which will be used to CTOR form JSON
 	};
 
-	static bool add(const char *, const FactoryFunction, const FactoryFunctionJson); ///< Adds a new Factory Pair derived from args to lookup table
+	static bool add(const char*, const FactoryFunction, const FactoryFunctionJson); ///< Adds a new Factory Pair derived from args to lookup table
 	
 	template <class U>
-	static bool add(const char *);
+	static bool add(const char*);
 	
 	template <class U>
-	static bool addNoJson(const char *);
+	static bool addNoJson(const char*);
 	
 	template <class U>
-	static bool addNoDefault(const char *);
+	static bool addNoDefault(const char*);
 
-	static T *create(const char *, LoomManager *scope); ///< Returns an instance of type T created by using the FactoryFunction coresponding to the provided name
-	static T *create(JsonVariant, LoomManager *);		///< Returns an instance of type T created from provided JSON
+	static T* create(const char*, LoomManager *scope); ///< Returns an instance of type T created by using the FactoryFunction coresponding to the provided name
+	static T* create(JsonVariant, LoomManager *);		///< Returns an instance of type T created from provided JSON
+
+	static void print_registry();
 
 private:
+
 	/// Use Meyer's singleton to prevent SIOF
-	static std::vector<FactoryPair> &getFactoryTable();
+	static std::vector<FactoryPair>& getFactoryTable();
 };
 
+///////////////////////////////////////////////////////////////////////////////
 // Registers a module that hasn't implemented a default constructor
 template <typename T>
 template <class U>
-bool Registry<T>::addNoDefault(const char *name)
+bool Registry<T>::addNoDefault(const char* name)
 {
 	return add(name, nullptr, ConstructDefaultJson<T, U>);
 }
 
+///////////////////////////////////////////////////////////////////////////////
 // Registers a module that hasn't implemented a JSON constructor
 template <typename T>
 template <class U>
-bool Registry<T>::addNoJson(const char *name)
+bool Registry<T>::addNoJson(const char* name)
 {
 	return add(name, ConstructDefault<T, U>, nullptr);
 }
 
+///////////////////////////////////////////////////////////////////////////////
 template <typename T>
 template <class U>
-bool Registry<T>::add(const char *name)
+bool Registry<T>::add(const char* name)
 {
 	return add(name, ConstructDefault<T, U>, ConstructDefaultJson<T, U>);
 }
 
+///////////////////////////////////////////////////////////////////////////////
 template <typename T>
-bool Registry<T>::add(const char *name, const Registry<T>::FactoryFunction ctor, const Registry<T>::FactoryFunctionJson ctorJson)
+bool Registry<T>::add(const char* name, const Registry<T>::FactoryFunction ctor, const Registry<T>::FactoryFunctionJson ctorJson)
 {
 	auto lookUp = getFactoryTable();
-	for (auto elem : lookUp)
-	{
-		///Add string processing for fuzzy search and debug suggestions
-		if (!strcmp(name, elem.name))
-		{
-			///On match, fail. Items two items cannot have the same creation name;
-			return false;
+	for (auto elem : lookUp) {
+		// Add string processing for fuzzy search and debug suggestions?
+		if (!strcmp(name, elem.name)) {
+			return false; // On match, fail. Items two items cannot have the same creation name;
 		}
-	} ///< No match found, exiting for
+	} // No match found, exiting for
+
 	getFactoryTable().push_back(FactoryPair{name, ctor, ctorJson});
 	return true;
 }
 
+///////////////////////////////////////////////////////////////////////////////
 template <typename T>
-T *Registry<T>::create(const char *name, LoomManager* scope)
+T* Registry<T>::create(const char* name, LoomManager* scope)
 {
 	auto lookUp = getFactoryTable();
-	for (auto elem : lookUp)
-	{
-		///Add string processing for fuzzy search and debug suggestions
-		if (!strcmp(name, elem.name))
-		{
-			///On match, return a *new* item of type name
-			return elem.ctor(scope);
+	for (auto elem : lookUp) {
+		// Add string processing for fuzzy search and debug suggestions?
+		if (!strcmp(name, elem.name)) {
+			return elem.ctor(scope); // On match, return a *new* item of type name
 		}
-	} ///< No match found, exiting for
+	} // No match found, exiting for
 	return nullptr;
 }
 
+///////////////////////////////////////////////////////////////////////////////
 template <typename T>
-T *Registry<T>::create(JsonVariant target, LoomManager* scope)
+T* Registry<T>::create(JsonVariant target, LoomManager* scope)
 {
 	const char *name = target["name"].as<char *>();
 	auto lookUp = getFactoryTable();
 
-	for (auto elem : lookUp)
-	{
-		if (!strcmp(name, elem.name))
-		{
+	for (auto elem : lookUp) {
+		if (!strcmp(name, elem.name)) {
 			return elem.ctor(scope);
 		}
 	}
 	return nullptr;
 }
 
-/// This function explots C++'s treatment of static initialization, where the compiler only expects a single instance to ever exist
+///////////////////////////////////////////////////////////////////////////////
+/// This function exploits C++'s treatment of static initialization, where 
+/// the compiler only expects a single instance to ever exist 
 /// Due to this behavior, we can use this
 template <typename T>
-std::vector<typename Registry<T>::FactoryPair> &Registry<T>::getFactoryTable()
+std::vector<typename Registry<T>::FactoryPair>& Registry<T>::getFactoryTable()
 {
 	static std::vector<FactoryPair> lookUpTable;
 	return lookUpTable;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+#include <typeinfo>
+
+template <typename T>
+void Registry<T>::print_registry()
+{
+	auto lookUp = getFactoryTable();
+	LPrint(typeid(T).name()+2);
+	LPrintln(" Factory [", lookUp.size(), " classes]");
+
+	for (auto elem : lookUp) {
+		LPrintln(" - ", elem.name);
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
