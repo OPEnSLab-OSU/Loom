bf0cad90aece12ee2063559449de18bb0065cc49
Mr.Kangs <gykang00@gmail.com>
2020-09-23 09:22:49 -0700

    Change pin for K30 example for Hypnos

--- examples/Sensors/K30/K30.ino
+++ examples/Sensors/K30/K30.ino
@@ -1,17 +1,20 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+//
 // This is basic example to use the K30 sensor with Loom.
 // The K30 sensor will measure the CO2 from the range 0 to 10000 ppm and log to SD.
-
+//
 // In this example of program, it will measure the CO2 level and display on the Serial Monitor.
 // Note that the K30 sensor needs at least 6 minutes of warm up to get accurate measurements.
 // Therefore, you will get data values after 6 minutes have passed.
 // Inside this program, there is a timer that will run for 6 minutes before it starts measuring values. 
-
+//
 // As you might see, there are some syntax that came from outside of the Loom language, 
-// so take a look the link and the code carefully.  
+// so take a look the link and the code carefully. 
+// The related syntax code came from the following website 
 // https://learn.adafruit.com/using-atsamd21-sercom-to-add-more-spi-i2c-serial-ports/creating-a-new-serial
-
+//
+// You are also able to use Hypnos board with this sensor since it will be using different pins 
+//
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <Loom.h>
@@ -34,8 +37,8 @@ LoomFactory<
 
 LoomManager Loom{ &ModuleFactory };
 
-// Create Serial SERCOM for K30 Sensor: RX pin 12, TX pin 11
-Uart Serial2 = Uart(&sercom1, 12, 11, SERCOM_RX_PAD_3, UART_TX_PAD_0);
+// Create Serial SERCOM for K30 Sensor: RX pin 13, TX pin 11
+Uart Serial2 = Uart(&sercom1, 13, 11, SERCOM_RX_PAD_1, UART_TX_PAD_0);
 
 void setup() {
   
@@ -45,9 +48,9 @@ void setup() {
   Loom.parse_config(json_config);
   Loom.print_config();
 
-  //Assign pins 12 & 11 SERCOM functionality
+  //Assign pins 13 & 11 SERCOM functionality
   pinPeripheral(11, PIO_SERCOM);
-  pinPeripheral(12, PIO_SERCOM);
+  pinPeripheral(13, PIO_SERCOM);
   
   Loom.K30().set_serial(&Serial2);
 
