b4b588799699069079616827897539c9ff0e8fd0
Luke Goertzen <goertzel@oregonstate.edu>
2021-02-20 08:52:04 -0800

    Rename example folder

---
+++ examples/LoggingPlatforms/OLED/config_str.h
@@ -0,0 +1,1 @@
+-D LCONFIG='"{\'general\':{\'name\':\'Device\',\'instance\':1,\'interval\':2000},\'components\':[{\'name\':\'Analog\',\'params\':\'default\'},{\'name\':\'OLED\',\'params\':[true,300,0,16,2,6000,10,2]}]}"'
