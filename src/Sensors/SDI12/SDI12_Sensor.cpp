11c76d3108894ac52f0338195ee5c066a4379bf2
Will Richards <richawil@oregonstate.edu>
2021-10-14 15:50:57 -0700

    Started to implement SDI12, using SitkaNet's dirty implementation

--- src/Sensors/SDI12/SDI12_Sensor.cpp
+++ src/Sensors/SDI12/SDI12_Sensor.cpp
@@ -11,15 +11,65 @@
 #ifdef LOOM_INCLUDE_SENSORS
 
 #include "SDI12_Sensor.h"
+#include <Arduino.h>
 
 using namespace Loom;
 
 ///////////////////////////////////////////////////////////////////////////////
-SDI12Sensor::SDI12Sensor(
-		const char*				module_name,
-		const uint8_t			num_samples 
-	) 
-	: Sensor(module_name, num_samples) {}
+SDI12Sensor::SDI12Sensor(const char* module_name, const uint8_t	num_samples, int sdiPin) 
+: Sensor(module_name, num_samples) : SDI12Interface(sdiPin) {}
+
+// Basic .begin call to the SDI 12 interface
+void SDI12Sensor::power_up(){
+	SDI12Interface.begin();
+}
+
+//Scan over the available addresses finding ones that return some value when queried and adding them to the list of valid addresses
+void SDI12Sensor::scanAddresses(){
+	for (byte i = 'A'; i <= 'D'; i++){
+		if(SDI12Sensor::checkActive(i))
+			SDI12Sensor::setTaken(i);
+	}
+}
+
+// Check if there is a device at the address
+bool SDI12Sensor::checkActive(char i){
+
+	// Send a basic 'acknowledge' command to verify that there is a device listening there format: [address][!]
+	String ackCommand = "";
+	ackCommand = "";
+	ackCommand += i;
+	ackCommand += "!";
+
+	// Attempt to contact the sensor 3 times
+	for (int j =0; j < 3; j++){
+
+		// Send an acknowledge command and if there is any response from the sensor break out of the loop
+		SDI12Interface.sendCommand(ackCommand);
+		if(SDI12Interface.available()>1) break;
+	}
+
+	// If anything is returned we assume there is a sensor occupying that address
+	if(SDI12Inteface.available() > 2){
+		SDI12Inteface.clearBuffer();
+		return true;
+	}
+
+	// If not clear the buffer and return false
+	SDI12Interface.clearBuffer();
+	return false;
+}
+
+
+// Sets the bit in the proper location within the addressRegister to record that the sensor is active and taken
+bool setTaken(byte i){
+	bool initStatus = isTaken(i);
+	i = charToDec(i);
+	byte j = i / 8; // Byte #
+	byte k = i % 8; // Bit #
+	addressRegister[j] |= (1 << k)
+	return !initStatus;
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 
--- src/Sensors/SDI12/SDI12_Sensor.h
+++ src/Sensors/SDI12/SDI12_Sensor.h
@@ -12,55 +12,94 @@
 #pragma once
 
 #include "../Sensor.h"
+#include <SDI12.h>
+#include <Arduino.h>
 
 namespace Loom {
 
-///////////////////////////////////////////////////////////////////////////////
-///
-/// Abstract base class for SDI12 sensor modules.
-///
-/// @par Resources
-/// - [Module Documentation](https://openslab-osu.github.io/Loom/html/class_loom_s_d_i12_sensor.html)
-///	- [Hardware Support](https://github.com/OPEnSLab-OSU/Loom/wiki/Hardware-Support#sdi-12-sensors)
-///
-///////////////////////////////////////////////////////////////////////////////
-class SDI12Sensor : public Sensor
-{
+	///////////////////////////////////////////////////////////////////////////////
+	///
+	/// Abstract base class for SDI12 sensor modules.
+	///
+	/// @par Resources
+	/// - [Module Documentation](https://openslab-osu.github.io/Loom/html/class_loom_s_d_i12_sensor.html)
+	///	- [Hardware Support](https://github.com/OPEnSLab-OSU/Loom/wiki/Hardware-Support#sdi-12-sensors)
+	///
+	///////////////////////////////////////////////////////////////////////////////
+	class SDI12Sensor : public Sensor
+	{
 
-protected:
+		protected:
 
+			// Underlying serial interface
+			SDI12 SDI12Interface;
 
-public:
-	
-//=============================================================================
-///@name	CONSTRUCTORS / DESTRUCTOR
-/*@{*/ //======================================================================
+			// Register representing each sensors address connected to the sdi device
+			byte addressRegister[8] = {
+				0B00000000,
+				0B00000000,
+				0B00000000,
+				0B00000000,
+				0B00000000,
+				0B00000000,
+				0B00000000,
+				0B00000000
+			};
 
-	/// Constructor
-	/// @param[in]	module_name		Name of the module (provided by derived classes)
-	/// @param[in]	module_type		Type of the module (provided by derived classes)
-	/// @param[in]	num_samples		The number of samples to take and average
-	SDI12Sensor(
-			const char*				module_name,
-			const uint8_t			num_samples = 1
-		);
+			// Check if there is a activity at that set address
+			bool checkActive(char i);
 
-	/// Destructor
-	virtual ~SDI12Sensor() = default;
+			// Scan the address space setting active addresses as taken
+			void scanAddresses();
 
-//=============================================================================
-///@name	OPERATION
-/*@{*/ //======================================================================
+			// Checks if there is an active sensor on the address
+			bool isTaken(byte i);
 
-//=============================================================================
-///@name	PRINT INFORMATION
-/*@{*/ //======================================================================
+			// Sets the specified address to taken, returns false if already taken
+			bool setTaken(byte i);
 
-private:
+			//Convert the ascii characters to numbers between 0 and 61 inclusive to represent the 62 possible address
+			byte charToDec(char i);
 
-};
+		public:
+			
+			//=============================================================================
+			///@name	CONSTRUCTORS / DESTRUCTOR
+			/*@{*/ //======================================================================
 
-///////////////////////////////////////////////////////////////////////////////
+				/// Constructor
+				/// @param[in]	module_name		Name of the module (provided by derived classes)
+				/// @param[in]	module_type		Type of the module (provided by derived classes)
+				/// @param[in]	num_samples		The number of samples to take and average
+				/// @param[in]  sdiPin			Pin for the SDI12 serial to origininate from
+				SDI12Sensor(const char* module_name, const uint8_t num_samples = 1, int sdiPin);
+
+				/// Destructor
+				virtual ~SDI12Sensor() = default;
+
+			//=============================================================================
+			///@name	OPERATION
+			/*@{*/ //======================================================================
+
+				void measure() override;
+				void package(JsonObject json) override;
+				void power_up() override;
+				void power_down() override;
+
+			//=============================================================================
+			///@name	PRINT INFORMATION
+			/*@{*/ //======================================================================
+
+				void print_measurements() const override;
+
+		private:
+
+			
+
+	};
+
+	REGISTER(Module, SDI12_Sensor, "SD12");
+	///////////////////////////////////////////////////////////////////////////////
 
 }; // namespace Loom
 
