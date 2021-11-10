f94eeb5f04d0f42c7c5b6d1a7d99430f93299db2
Will Richards <richawil@oregonstate.edu>
2021-11-10 08:20:46 -0800

    added teros sensor functionality

---
+++ src/Sensors/SDI12/Teros.cpp
@@ -0,0 +1,122 @@
+///////////////////////////////////////////////////////////////////////////////
+///
+/// @file		Decagon_GS3.cpp
+/// @brief		File for Teros implementation.
+/// @author		Will Richards
+/// @date		2021
+/// @copyright	GNU General Public License v3.0
+///
+///////////////////////////////////////////////////////////////////////////////
+
+#ifdef LOOM_INCLUDE_SENSORS
+
+#include "Teros.h"
+#include "Module_Factory.h"
+#include <Arduino.h>
+
+using namespace Loom;
+
+///////////////////////////////////////////////////////////////////////////////
+Teros::Teros(const uint8_t addr, const uint8_t num_samples)
+	: SDI12Sensor(addr, "Teros", num_samples), pinAddr(addr)
+{
+	LMark;
+
+	// Try to get the first address in the address buffer
+	sensorAddresses = getTaken();
+
+
+	// Check if the sensor address actually changed
+	if(sensorAddresses.size() > 0 ){
+		Serial.print("\n=== Decagon initialized successfully ===\n");  
+  	} else {
+    	Serial.print("\n=== Decagon failed to initialize ===\n"); 
+  	}
+	
+}
+
+///////////////////////////////////////////////////////////////////////////////
+Teros::Teros(JsonArrayConst p)
+	: Teros(EXPAND_ARRAY(p, 2) ) {}
+
+///////////////////////////////////////////////////////////////////////////////
+void Teros::print_config() const
+{
+	SDI12Sensor::print_config();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void Teros::print_measurements() const
+{
+	print_module_label();
+	LPrintln("Measurements:");
+
+	for (int i = 0; i < sensorAddresses.size(); i++){
+		LPrintln("Device #", i);
+		LPrintln("\tTemperature: ", temp[i]);
+		LPrintln("\tMoisture: ", moisture[i]);
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void Teros::measure()
+{
+	LMark;
+	
+	// Loop over the SDI addresses and pull measurement data from each sensor
+	for (char i : sensorAddresses){
+		// Measure the data from the sensor
+		sendCommand(i, "M!");
+		LMark;
+
+		// Poll data from the sensor
+		sdiResponse = sendCommand(i, "D0!");
+
+		LMark;
+		parse_results();
+	}
+	LMark;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void Teros::package(JsonObject json)
+{
+	LMark;
+	JsonObject data = get_module_data_object(json, module_name);
+
+	for (int i = 0; i < sensorAddresses.size(); i++){
+		String sensorInst = String("Sensor #") + String(i);
+		data[sensorInst]["moisture"]	= moisture[i];
+		data[sensorInst]["temperature"]	= temp[i];
+	}
+}
+
+/**
+ * Called by the module manager when the feather needs to wake back up
+ */ 
+void Teros::power_up(){
+	pinMode(pinAddr, OUTPUT);
+}
+
+
+/**
+ * Called by the module manager when the feather is sleeping
+ */ 
+void Teros::power_down(){
+	pinMode(pinAddr, INPUT);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void Teros::parse_results(){
+	sdiResponse.toCharArray(buf, sizeof(buf));
+	p = buf;
+
+	// Read out the results and parse out each of the data readings and pares them to floats
+	strtok_r(p, "+", &p);
+	moisture.push_back(atof(strtok_r(NULL, "+", &p)));
+	temp.push_back(atof(strtok_r(NULL, "+", &p)));
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // ifdef LOOM_INCLUDE_SENSORS
---
+++ src/Sensors/SDI12/Teros.h
@@ -0,0 +1,99 @@
+///////////////////////////////////////////////////////////////////////////////
+///
+/// @file		Teros.h
+/// @brief		File for Teros definition.
+/// @author		Will Richards
+/// @date		2021
+/// @copyright	GNU General Public License v3.0
+///
+///////////////////////////////////////////////////////////////////////////////
+
+#ifdef LOOM_INCLUDE_SENSORS
+#pragma once
+
+#include "SDI12_Sensor.h"
+#include <SDI12.h>
+#include <vector>
+
+namespace Loom {
+
+///////////////////////////////////////////////////////////////////////////////
+///
+/// Teros soil moisture sensor
+///
+/// @par Resources
+/// - [Module Documentation](https://openslab-osu.github.io/Loom/html/class_loom___decagon_g_s3.html)
+///	- [Hardware Support](https://github.com/OPEnSLab-OSU/Loom/wiki/Hardware-Support#sdi-12-sensors)
+///
+///////////////////////////////////////////////////////////////////////////////
+class Teros : public SDI12Sensor
+{
+
+protected:
+
+	uint8_t		pinAddr;		// Address of the sensor
+
+	std::vector<float>		moisture;	// Moisture reading
+	std::vector<float>		temp;		// Temperature reading
+	
+	String		sdiResponse = "";
+	String		myCommand = "";
+	char		buf[20];
+	char*		p;
+	std::vector<char>		sensorAddresses;
+
+	void parse_results();
+
+
+public:
+	
+//=============================================================================
+///@name	CONSTRUCTORS / DESTRUCTOR
+/*@{*/ //======================================================================
+
+	/// Decagon GS3 module constructor
+	///
+	/// @param[in]	num_samples			Set(Int) | <8> | {1, 2, 4, 8, 16} | How many samples to take and average
+	Teros(
+			const uint8_t	pinAddr = 11,
+			const uint8_t	num_samples	= 1
+		);
+
+	/// Constructor that takes Json Array, extracts args
+	/// and delegates to regular constructor
+	/// @param[in]	p		The array of constuctor args to expand
+	Teros(JsonArrayConst p);
+
+	/// Destructor
+	~Teros() = default;
+
+//=============================================================================
+///@name	OPERATION
+/*@{*/ //======================================================================
+
+	void		measure() override;
+	void		package(JsonObject json) override;
+
+	void		power_up() override;
+	void		power_down() override;
+
+//=============================================================================
+///@name	PRINT INFORMATION
+/*@{*/ //======================================================================
+
+	void		print_config() const override;
+	void		print_measurements() const override;
+
+private:
+
+};
+
+///////////////////////////////////////////////////////////////////////////////
+REGISTER(Module, Teros, "Teros");
+///////////////////////////////////////////////////////////////////////////////
+
+}; // namespace Loom
+
+#endif // ifdef LOOM_INCLUDE_SENSORS
+
+
